5020ffa1ab1c37caeae6d5cc399edeac7356172e
Blake Williams <duckbill43@gmail.com>
2019-11-16 22:08:57 -0500

    got the clock working, but i dont know how to update the format data member once its already rendered.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Clock extends React.Component {
+  constructor(props){
+    super();
+    this.format = props.format;
+    this.state = {time: new Date().toLocaleTimeString()};
+  }
+
+  componentDidMount(){
+    this.intervalId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
+
+  tick(){
+    if (this.format === "time"){
+      this.setState({time: new Date().toLocaleTimeString()});
+    } else if (this.format === "date") {
+      this.setState({time: new Date().toDateString()});
+    }
+  }
+
+  render(){
+    if (this.format === "time"){
+      return (
+        <p>{new Date().toLocaleTimeString()}</p>
+      );
+    } else if (this.format === "date") {
+      return (
+        <p>{new Date().toDateString()}</p>
+      );
+    }
+  }
+}
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {format: "time"};
+    this.formatCycle = ["time", "date", "all"];
+    this.cycleCursor = 1;
+  }
+
+  changeFormat(){
+    let index = this.cycleCursor%this.formatCycle.length;
+    let formatType = this.formatCycle[index];
+    let newState = {format: formatType};
+    this.setState(newState);
+    this.cycleCursor += 1;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Clock format={this.state.format}/>
+          <Button variant="primary" size="lg" onClick={() => this.changeFormat()}>
+            {this.state.format}
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
